1fc9ec057848883a8e45758455e69dbf51ecb7ab
lshen365 <lshen1905@gmail.com>
2019-06-16 20:33:31 -0600

    Removed Send File functionality

---
+++ SendFile.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+using namespace std;
+
+class SendFile
+{
+public:
+	SendFile();
+	~SendFile();
+
+private:
+
+};
+
+SendFile::SendFile()
+{
+}
+
+SendFile::~SendFile()
+{
+}
