33f6d9da1ea03665cd4fc50dfed7e179373c1b62
lshen365 <34820515+lshen365@users.noreply.github.com>
2020-09-17 21:47:59 -0600

    Update KeyLogger.cpp
    Cleaned up some unused comments/code

--- KeyLogger.cpp
+++ KeyLogger.cpp
@@ -3,13 +3,6 @@
 #include <Windows.h>
 #include <iostream>
 using namespace std;
-
-
-
-//string isNumber(DWORD word) { //Checks if the key stroke is a number or a letter
-//	
-//
-//}
 string currentSentence = "";
 void findLetterTyped() {
 
@@ -39,14 +32,3 @@ int main()
 		findLetterTyped();
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
